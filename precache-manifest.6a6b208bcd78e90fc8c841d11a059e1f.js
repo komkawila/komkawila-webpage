5705bdd024c1bd5b02768b9f6ad07337b7b8edf3
komkawila <kompanuwat@gmail.com>
2022-01-02 00:09:33 +0700

    first commit

---
+++ precache-manifest.6a6b208bcd78e90fc8c841d11a059e1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f05d68bcbfe20a4a3ad23fa576e218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70868b4234a64d811eca",
+    "url": "/static/css/main.61c26774.chunk.css"
+  },
+  {
+    "revision": "5120685eb90fb5b5113b",
+    "url": "/static/js/2.543286d7.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.543286d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70868b4234a64d811eca",
+    "url": "/static/js/main.a4d53737.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  }
+]);
